3caa3832e1cc73705a63cd7c55d9375df759e358
aivalle <aivalle1985@gmail.com>
2018-09-27 17:37:38 -0500

    adding Google Play Services

---
+++ Assets/Scripts/Misc/GooglePlay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using GooglePlayGames;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class GooglePlay : MonoBehaviour {
+	public GameObject BtnAc, BtnLB;
+
+	private void Awake(){
+		PlayGamesPlatform.Activate ();
+		if(PlayGamesPlatform.Instance.localUser.authenticated){
+			Debug.Log ("Logged Already");
+			BtnAc.SetActive (true); 
+			BtnLB.SetActive (true);
+		}else{
+			BtnAc.SetActive (false); 
+			BtnLB.SetActive (false);
+			Social.localUser.Authenticate((bool success) =>
+				{
+					Debug.Log("Logged in");
+				});
+		}
+	}
+}
