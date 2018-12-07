9bcbb6d0d56d3a06ee2821d2286839441eb7645c
aivalle <aivalle1985@gmail.com>
2018-12-06 21:29:34 -0600

    adding Unity Ads

---
+++ Assets/Scripts/Misc/UnityAds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityAds : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		Advertisement.Initialize("2822114");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RequestVideo(){
+		ShowOptions so = new ShowOptions ();
+		so.resultCallback = getReward;
+		Advertisement.Show ("rewardedVideo",so);
+	}
+
+	private void getReward(ShowResult sr){
+		if (sr == ShowResult.Finished) {
+			Debug.Log ("Ad finish successfully");
+		} else {
+			Debug.Log ("Ad not completed");
+		}
+	}
+}
